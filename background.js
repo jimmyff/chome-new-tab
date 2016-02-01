36ba9919421acc57b4ff8374db566262d78cfa12
Jimmy Forrester-Fellowes <jimmy@rocketware.co.uk>
2016-02-01 20:21:31 +0000

    Fixed minor whitespace inconsistency

--- background.js
+++ background.js
@@ -1,21 +1,20 @@
 // add listener
 chrome.tabs.onCreated.addListener(function(tabObj) {
 
-	var tabId = tabObj.id;
- 	if (tabObj.url === 'chrome://newtab/') {
+  var tabId = tabObj.id;
+  if (tabObj.url === 'chrome://newtab/') {
 
-		// load URL
-		chrome.storage.sync.get("url", function(items) {
+    // load URL
+    chrome.storage.sync.get("url", function(items) {
 
-			// update chrome tab URL if valid data
-			if (!chrome.runtime.error && items.url)
-				 chrome.tabs.getCurrent(function(tab) {
-             chrome.tabs.update(
-							 tabId,
-							 { url:items.url }
-						 );
-          });
-		});
-	}
-
+      // update chrome tab URL if valid data
+      if (!chrome.runtime.error && items.url)
+        chrome.tabs.getCurrent(function(tab) {
+            chrome.tabs.update(
+              tabId,
+              { url:items.url }
+            );
+        });
+    });
+  }
 });
--- options.js
+++ options.js
@@ -1,68 +1,68 @@
 // Clear URL from storage
-var clearUrl  = function (){
-	 chrome.storage.sync.remove('url', function(){
-			window.dispatchEvent(
-				new CustomEvent('url:loaded', { 'detail': false })
-			);
-	 });
+var clearUrl  = function () {
+   chrome.storage.sync.remove('url', function(){
+      window.dispatchEvent(
+        new CustomEvent('url:loaded', { 'detail': false })
+      );
+   });
  }
 
 // Update URL
 var writeUrl = function (newUrl) {
-	chrome.storage.sync.set({ "url" : newUrl }, function() {
+  chrome.storage.sync.set({ "url" : newUrl }, function() {
       if (chrome.extension.lastError) {
-			   alert('Can not write URL: ' + chrome.extension.lastError.message);
-			} else {
-					window.dispatchEvent(
-						new CustomEvent('url:loaded', { 'detail': newUrl })
-					);
-		  }
-	});
+         alert('Can not write URL: ' + chrome.extension.lastError.message);
+      } else {
+          window.dispatchEvent(
+            new CustomEvent('url:loaded', { 'detail': newUrl })
+          );
+      }
+  });
 }
 
 // Update UI
 var updateUrlUi = function (storedUrl) {
-	console.log('update UI got URL: '+ storedUrl);
-	if (storedUrl) {
-			document.getElementById('inputUrl').value = storedUrl;
-			document.getElementById("clearUrl").style.visibility = "visible";
-	} else {
-			 document.getElementById('inputUrl').value = "";
-			 document.getElementById("clearUrl").style.visibility = "hidden";
-	}
+  console.log('update UI got URL: '+ storedUrl);
+  if (storedUrl) {
+      document.getElementById('inputUrl').value = storedUrl;
+      document.getElementById("clearUrl").style.visibility = "visible";
+  } else {
+       document.getElementById('inputUrl').value = "";
+       document.getElementById("clearUrl").style.visibility = "hidden";
+  }
 }
 
 // Load URL
 var getUrlFromStorage = function () {
-	chrome.storage.sync.get("url", function(items) {
-			if (!chrome.runtime.error) {
-				if (items.url)
-					window.dispatchEvent(
-						new CustomEvent('url:loaded', { 'detail': items.url })
-					);
-				return items.url;
-			}
-	});
+  chrome.storage.sync.get("url", function(items) {
+      if (!chrome.runtime.error) {
+        if (items.url)
+          window.dispatchEvent(
+            new CustomEvent('url:loaded', { 'detail': items.url })
+          );
+        return items.url;
+      }
+  });
 }
 
 window.addEventListener('load', function(evt) {
 
-		// load the URL from storage and update the UI...
-		getUrlFromStorage();
+    // load the URL from storage and update the UI...
+    getUrlFromStorage();
 
-		// listen for URL updates...
-		window.addEventListener('url:loaded', function (e) {
-			console.log('got url loaded... '+e.detail);
-			console.log(e);
-				updateUrlUi(e.detail);
-		});
+    // listen for URL updates...
+    window.addEventListener('url:loaded', function (e) {
+      console.log('got url loaded... '+e.detail);
+      console.log(e);
+        updateUrlUi(e.detail);
+    });
 
-		// Handle save
-	  document.getElementById('buttonSave').addEventListener('click', function (e) {
-			writeUrl(document.getElementById('inputUrl').value);
-		});
+    // Handle save
+    document.getElementById('buttonSave').addEventListener('click', function (e) {
+      writeUrl(document.getElementById('inputUrl').value);
+    });
 
-		// Handle clear
-  	document.getElementById('buttonClear').addEventListener('click', clearUrl);
+    // Handle clear
+    document.getElementById('buttonClear').addEventListener('click', clearUrl);
 
 });
