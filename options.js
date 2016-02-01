4b4b21e334781f3c2608d669acb865bf86888047
Jimmy Forrester-Fellowes <jimmy@rocketware.co.uk>
2016-02-01 20:40:53 +0000

    tidy up

--- options.js
+++ options.js
@@ -1,22 +1,32 @@
+// Fetch URL from storage
+var getUrlFromStorage = function () {
+  chrome.storage.sync.get("url", function(items) {
+    if (!chrome.runtime.error && items.url)
+      window.dispatchEvent(
+        new CustomEvent('url:loaded', { 'detail': items.url })
+      );
+  });
+}
+
 // Clear URL from storage
 var clearUrl  = function () {
-   chrome.storage.sync.remove('url', function(){
-      window.dispatchEvent(
-        new CustomEvent('url:loaded', { 'detail': false })
-      );
-   });
- }
+  chrome.storage.sync.remove('url', function(){
+    window.dispatchEvent(
+      new CustomEvent('url:loaded', { 'detail': false })
+    );
+  });
+}
 
-// Update URL
+// Write URL to storage
 var writeUrl = function (newUrl) {
   chrome.storage.sync.set({ "url" : newUrl }, function() {
-      if (chrome.extension.lastError) {
-         alert('Can not write URL: ' + chrome.extension.lastError.message);
-      } else {
-          window.dispatchEvent(
-            new CustomEvent('url:loaded', { 'detail': newUrl })
-          );
-      }
+    if (chrome.extension.lastError) {
+       alert('Can not save URL: ' + chrome.extension.lastError.message);
+    } else {
+      window.dispatchEvent(
+        new CustomEvent('url:loaded', { 'detail': newUrl })
+      );
+    }
   });
 }
 
@@ -28,43 +38,31 @@ var updateUrlUi = function (storedUrl) {
     "visible":"hidden";
 }
 
-// Load URL
-var getUrlFromStorage = function () {
-  chrome.storage.sync.get("url", function(items) {
-      if (!chrome.runtime.error) {
-        if (items.url)
-          window.dispatchEvent(
-            new CustomEvent('url:loaded', { 'detail': items.url })
-          );
-        return items.url;
-      }
-  });
-}
 
 window.addEventListener('load', function(evt) {
 
-    // load the URL from storage and update the UI...
-    getUrlFromStorage();
+  // load the URL from storage and update the UI...
+  getUrlFromStorage();
 
-    // listen for URL updates...
-    window.addEventListener('url:loaded', function (e) {
-      console.log('got url loaded... '+e.detail);
-      console.log(e);
-        updateUrlUi(e.detail);
-    });
+  // listen for URL updates...
+  window.addEventListener('url:loaded', function (e) {
+    console.log('got url loaded... '+e.detail);
+    console.log(e);
+      updateUrlUi(e.detail);
+  });
 
-    // Handle save
-    document.getElementById('buttonSave').addEventListener(
-      'click',
-      function (e) {
-        writeUrl(document.getElementById('inputUrl').value);
-      }
-    );
+  // Handle save
+  document.getElementById('buttonSave').addEventListener(
+    'click',
+    function (e) {
+      writeUrl(document.getElementById('inputUrl').value);
+    }
+  );
 
-    // Handle clear
-    document.getElementById('buttonClear').addEventListener(
-      'click',
-      clearUrl
-    );
+  // Handle clear
+  document.getElementById('buttonClear').addEventListener(
+    'click',
+    clearUrl
+  );
 
 });
